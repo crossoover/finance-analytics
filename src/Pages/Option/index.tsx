8d469e701fe5c509c64ae7cb37e7427c5501fa8a
Viacheslav Savratskyi <savratskywork@gmail.com>
2023-02-20 13:28:02 +0200

    minor refactoring

--- src/Pages/Option/index.tsx
+++ src/Pages/Option/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @typescript-eslint/no-unused-expressions */
 /* eslint-disable react-hooks/rules-of-hooks */
 /* eslint-disable consistent-return */
--- src/components/RippleAnimation/index.tsx
+++ src/components/RippleAnimation/index.tsx
@@ -1,20 +1,22 @@
-import { FC } from 'react'
-import { CirclesWrap, Circle } from './styles'
-import { CIRCLE_CONFIG_MAP, MIN_WIDTH_BREAKPOINT } from '../../utils/constants'
+/* eslint-disable prettier/prettier */
+import { FC } from 'react';
+import { CirclesWrap, Circle } from './styles';
+import { CIRCLE_CONFIG_MAP, MIN_WIDTH_BREAKPOINT } from '../../utils/constants';
 
 export type ICircle = {
-  size: number
-  duration: number
-  opacity: number
-}
+	size: number;
+	duration: number;
+	opacity: number;
+};
 
 const RippleAnimation: FC = () =>
-  window.innerWidth > MIN_WIDTH_BREAKPOINT ? (
-    <CirclesWrap>
-      {CIRCLE_CONFIG_MAP.map((circle) => (
-        <Circle key={circle.size} {...circle} />
-      ))}
-    </CirclesWrap>
-  ) : null
+	window.innerWidth > MIN_WIDTH_BREAKPOINT ? (
+		<CirclesWrap>
+			{CIRCLE_CONFIG_MAP.map((circle) => (
+				// eslint-disable-next-line react/jsx-props-no-spreading
+				<Circle key={circle.size} {...circle} />
+			))}
+		</CirclesWrap>
+	) : null;
 
-export default RippleAnimation
+export default RippleAnimation;
